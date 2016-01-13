0a650190f229cd51adfc404ee6f12491b6867498
Oran Luzon <oran.luzon@gmail.com>
2016-01-13 16:20:05 -0500

    initial commit

---
+++ ScoutingApp/LogIn.swift
@@ -0,0 +1,18 @@
+//
+//  LogIn.swift
+//  ScoutingApp
+//
+//  Created by Oran Luzon on 1/13/16.
+//  Copyright © 2016 Oran Luzon. All rights reserved.
+//
+
+import UIKit
+
+class LogIn: NSObject{
+    var user: String!
+    var pass: String!
+    
+    init(){
+        
+    }
+}
